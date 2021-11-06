777fd10b134a2d41c3e52dc3eebb29fc236ef6ae
Louis Agyapong <agyapong.louis@gmail.com>
2021-11-06 12:40:31 +0000

    pytest fixtures

--- tests/test_example_one.py
+++ tests/test_example_one.py
@@ -1,6 +1,30 @@
 import pytest
 
 
+@pytest.fixture(scope="module")
+def fixture_one():
+    print("run fixture one 🛠👨🏾‍🔧")
+    return 1
+
+
+def test_example_two(fixture_one):
+    print("run test example two 🧪🔬")
+    num = fixture_one
+    assert num == 1
+
+
+def test_example_three(fixture_one):
+    print("run test example three 🧪🔬")
+    num = fixture_one
+    assert num == 1
+
+
+def test_example_four(fixture_one):
+    print("run test example four 🧪🔬")
+    num = fixture_one
+    assert num == 1
+
+
 @pytest.mark.xfail
 def test_example():
     assert 1 == 1
